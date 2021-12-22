1114d4ac4de294c98049f28a29c629c8d264b642
mokoshin0720 <shinya.work0720@gmail.com>
2021-12-22 14:24:29 +0900

    [add]dataloader

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from train.config import *
-from data.df import * 
+from data.df import *
 from data.dataset import * 
+from data.loader import *
 
 if __name__ == "__main__":
     # train('microsoft/DialoGPT-small', 100)
     model, tokenizer, optimizer = prepare_train('microsoft/DialoGPT-small')
     df = prepare_df()
-    d = MyDataset(df, tokenizer)
-    print(d[0])
-    print(len(d))
+    dataset = MyDataset(df, tokenizer)
+    tmp = dataset[0]
+    decoded1 = tokenizer.decode(tmp[0].tolist()[0])
+    decoded2 = tokenizer.decode(tmp[1].tolist()[0])
+    print(decoded1)
+    print(decoded2)
+    # prepare_loader(dataset)
