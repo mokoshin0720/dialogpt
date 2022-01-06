cda3c8e39aeb8a12f36a5d92a01d75257133280d
mokoshin0720 <shinya.work0720@gmail.com>
2022-01-06 15:53:40 +0900

    [add]confirm runnig

--- src/train/main.py
+++ src/train/main.py
@@ -1,54 +1,35 @@
 from . import config
 import torch
 
-def train(model_name: str, epochs: int):
-    model, tokenizer, optimizer = config.prepare_train(model_name)
-    text_1 = "Who was Jim Henson is is ?"
-    text_2 = "Jim Henson was a puppeteer"
-    encoded_1 = tokenizer.encode(text_1)
-    encoded_2 = tokenizer.encode(text_2)
-    
-    model.train()
+def model_train(model, tokenizer, optimizer, train_dataloader):
+    epochs = 10
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
     for epoch in range(0, epochs):
         print("")
         print('======== Epoch {:} / {:} ========'.format(epoch + 1, epochs))
         print('Training...')
 
-        in_text = torch.tensor(encoded_1)
-        out_text = torch.tensor(encoded_2)
+        total_train_loss = 0
+        model.train()
+        
+        for batch in train_dataloader:
+            context, reply = batch
+            in_text = context.to(device)
+            out_text = reply.to(device)
 
+            optimizer.zero_grad()
 
-        outputs = model(in_text, labels=out_text)
-        loss = outputs[0]
+            outputs = model(in_text, labels=out_text)
+            loss = outputs[0]
 
-        optimizer.zero_grad()
-        loss.backward()
+            batch_loss = loss.item()
+            total_train_loss += batch_loss
 
-        optimizer.step()
-
-        print(loss)
-
-# def model_train():
-    # for epoch in range(0, epochs):
-    #     print("")
-    #     print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-    #     print('Training...')
-
-    #     total_train_loss = 0
-    #     model.train()
-
-    #     for step, batch in enumerate(train_dataloader):
-    #         in_text = batch[?].to(device)
-    #         out_text = batch[?].to(device)
-
-    #         model.zero_grad()
-
-    #         outputs = model(in_text, labels=out_text)
-    #         loss = outputs[0]
-
-    #         batch_loss = loss.item()
-    #         total_train_loss += batch_loss
-
-    #         loss.backward()
-    #         optimizer.step()
-    #         scheduler.step()
+            loss.backward()
+            optimizer.step()
+            # scheduler.step()
+            # print(loss)
+            
+    model_path = 'model.pth'
+    torch.save(model.state_dict(), model_path)
