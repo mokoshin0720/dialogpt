1e730c62d6dd4e1d2be5836c3b62694219338df2
mokoshin0720 <shinya.work0720@gmail.com>
2022-01-10 16:31:10 +0900

    [add]evaluate

---
+++ src/evaluate/main.py
@@ -0,0 +1,42 @@
+import torch
+
+def model_eval(model, tokenizer, optimizer, eval_dateloader):
+    load_path = '../../model.pth'
+    load_weights = torch.load(load_path)
+    model.load_state_dict(load_weights)
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    
+    model.eval()
+        
+    for batch in train_dataloader:
+        context, reply = batch
+        in_text = context.to(device)
+        out_text = reply.to(device)
+        optimizer.zero_grad()
+        outputs = model(in_text, labels=out_text)
+        loss = outputs[0]
+        batch_loss = loss.item()
+        total_train_loss += batch_loss
+        loss.backward()
+        optimizer.step()
+
+def print_output(tokenizer, model):
+    load_path = 'src/evaluate/model.pth'
+    load_weights = torch.load(load_path)
+    model.load_state_dict(load_weights)
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    
+    model.eval()
+    
+    
+    input_sentence = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+    output_sentence = model.generate(
+    input_sentence, max_length=1000,
+    pad_token_id=tokenizer.eos_token_id
+    )
+    reply = tokenizer.decode(output_sentence, skip_special_tokens=True)
+    print("DialoGPT: {}".format(reply))
--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ from train.main import *
 from data.df import *
 from data.dataset import * 
 from data.loader import *
+from evaluate.main import *
 
+# if __name__ == "__main__":
+#     model, tokenizer, optimizer = prepare_train('microsoft/DialoGPT-small')
+#     df = prepare_df()
+#     dataset = MyDataset(df, tokenizer)
+#     train_loader = prepare_loader(dataset)
+#     model_train(
+#         model=model,
+#         tokenizer=tokenizer,
+#         optimizer=optimizer,
+#         train_dataloader=train_loader,
+#     )
+    
 if __name__ == "__main__":
     model, tokenizer, optimizer = prepare_train('microsoft/DialoGPT-small')
-    df = prepare_df()
-    dataset = MyDataset(df, tokenizer)
-    train_loader = prepare_loader(dataset)
-    model_train(
-        model=model,
-        tokenizer=tokenizer,
-        optimizer=optimizer,
-        train_dataloader=train_loader,
-    )
+    print_output(tokenizer=tokenizer, model=model)
